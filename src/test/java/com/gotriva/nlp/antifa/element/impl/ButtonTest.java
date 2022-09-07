26397673be00f61131a18f41c0153d4be90c038a
Fábio da Silva Júnior <fabiolavisjunior@gmail.com>
2022-09-07 17:19:39 -0300

    Fixed Button test

--- src/test/java/com/gotriva/nlp/antifa/element/impl/ButtonTest.java
+++ src/test/java/com/gotriva/nlp/antifa/element/impl/ButtonTest.java
@@ -8,8 +8,8 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.By.ById;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 /** Test for {@link Button} class. */
@@ -23,13 +23,13 @@ public class ButtonTest {
   private Button button;
 
   @BeforeEach
-  public void setup() {
+  public void init() {
     /** Initialize selenium driver */
     driver = new ChromeDriver();
   }
 
   @AfterEach
-  public void tearDown() {
+  public void close() {
     /** Clise selenium driver */
     driver.close();
   }
@@ -37,45 +37,51 @@ public class ButtonTest {
   @Test
   public void test_ClickButtonTest1_then_WriteMessageButtonTest1WasClicked() {
     /** Generate test page for button */
-    stubFor(
-        get(urlEqualTo("/button-1-test"))
-            .willReturn(
-                aResponse()
-                    .withHeader("Content-Type", "text/html")
-                    .withBody(
-                        "<html><body><button id=\"test1\" onclick=\"document.write('button test1"
-                            + " was clicked')\">Test 1</button></body></html>")));
-    /** Initialize button */
-    driver.get("http://localhost:8001/button-1-test");
-    button = new Button(driver.findElement(ById.id("test1")));
+    String path = "/button-1-test";
+    String html =
+        "<html><body><button id=\"test1\" onclick=\"document.write('button test1"
+            + " was clicked')\">Test 1</button></body></html>";
+    mockPage(path, html);
+    navigateTo(path);
 
+    button = new Button(findElementById("test1"));
     button.click();
 
-    assertTrue(
-        () ->
-            driver.findElement(By.tagName("body")).getText().endsWith("button test1 was clicked"));
+    assertTrue(() -> findElementByTag("body").getText().endsWith("button test1 was clicked"));
   }
 
   @Test
   public void test_ClickButtonTest2_then_WriteMessageButtonTest2WasClicked() {
     /** Generate test page for button */
-    stubFor(
-        get(urlEqualTo("/button-2-test"))
-            .willReturn(
-                aResponse()
-                    .withHeader("Content-Type", "text/html")
-                    .withBody(
-                        "<html><body><input id=\"test1\" onclick=\"document.write('button test2 was"
-                            + " clicked')\" type=\"button\" value=\"Test"
-                            + " 2\"/></button></body></html>")));
-    /** Initialize button */
-    driver.get("http://localhost:8001/button-2-test");
-    button = new Button(driver.findElement(ById.id("test1")));
+    String path = "/button-2-test";
+    String html =
+        "<html><body><input id=\"test2\" onclick=\"document.write('button test2 was"
+            + " clicked')\" type=\"button\" value=\"Test"
+            + " 2\"/></button></body></html>";
+    mockPage(path, html);
+    navigateTo(path);
 
+    button = new Button(findElementById("test2"));
     button.click();
 
-    assertTrue(
-        () ->
-            driver.findElement(By.tagName("body")).getText().endsWith("button test2 was clicked"));
+    assertTrue(() -> findElementByTag("body").getText().endsWith("button test2 was clicked"));
+  }
+
+  private void navigateTo(String path) {
+    driver.get("http://localhost:8001" + path);
+  }
+
+  private WebElement findElementById(String id) {
+    return driver.findElement(By.id(id));
+  }
+
+  private WebElement findElementByTag(String tag) {
+    return driver.findElement(By.tagName("body"));
+  }
+
+  private void mockPage(String path, String html) {
+    stubFor(
+        get(urlEqualTo(path))
+            .willReturn(aResponse().withHeader("Content-Type", "text/html").withBody(html)));
   }
 }
